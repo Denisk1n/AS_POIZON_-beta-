392c4670dec74bae461233f7982f6a486ccb515c
Deniskin_PC <drewbs.1@mail.ru>
2024-12-22 16:39:41 +0300

    Джинджа шаблонизатор и рапределение кода по файлам + block

--- ASPoizon/main/views.py
+++ ASPoizon/main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-   return HttpResponse("<h1> Проверка работы </h1>")
+   return render(request, 'main/index.html')
 
 
 def about(request):
-   return HttpResponse("<h2> Не много информации о нас :)</h2>")
+   return render(request, 'main/about.html')
